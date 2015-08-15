fa8eb9472516c87b0ca152502e3b3413aca5385f
Rafael Viana <rafael.cunha@al.infnet.edu.br>
2015-08-15 12:31:02 -0300

    Padrão de projeto Strategy

---
+++ src/CalculadorDeImpostos.java
@@ -0,0 +1,9 @@
+
+public class CalculadorDeImpostos {
+	
+	public void realizaCalculo(Orcamento orcamento, Imposto imposto) {
+		double icms = imposto.calcula(orcamento);
+		System.out.println(icms);
+	}
+
+}
---
+++ src/Orcamento.java
@@ -0,0 +1,14 @@
+
+public class Orcamento {
+	private double valor;
+
+	public Orcamento(double valor) {
+		super();
+		this.valor = valor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+}
